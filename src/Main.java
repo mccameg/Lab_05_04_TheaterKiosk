f02c7af8a3921313852ddd7016c8f2cb84540000
mccameg <meganmccarren@yahoo.com>
2023-02-12 20:42:02 -0500

    Final

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 	int DRINKING_AGE = 21;
-    int age = 0;
+    int age = 25;
 
-    if (age >= DRINKING_AGE)
+    if (age >= DRINKING_AGE) // if age is greater than or equal to 21, the output below will display
         {
-            System.out.println("You get a wrist band.");
+            System.out.println("You get a wrist band."); // output will tell user that they get a wrist band if age is 21 or greater
         }
     }
 }
