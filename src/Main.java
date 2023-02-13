8973486a0a6bc70f1efe1be6d2b0ebd5ab3d37eb
mccameg <meganmccarren@yahoo.com>
2023-02-12 20:38:22 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int DRINKING_AGE = 21;
+    int age = 0;
+
+    if (age >= DRINKING_AGE)
+        {
+            System.out.println("You get a wrist band.");
+        }
+    }
+}
